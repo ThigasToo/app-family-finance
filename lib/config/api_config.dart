0b5ff1f6f5587c61463c4ddff64d07ef123cf668
Thiago <rcms2015@hotmail.com>
2026-08-27 11:39:50 -0300

    Atualizando app

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,5 +2,5 @@ class ApiConfig {
   // Chrome / Windows Desktop → localhost funciona direto
   // Emulador Android → trocar para 10.0.2.2
   // Celular físico → trocar para o IP local do seu PC (ex: 192.168.1.50)
-  static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "http://10.0.2.2:8000";
 }
